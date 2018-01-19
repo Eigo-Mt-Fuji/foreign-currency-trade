a9df788309c374350bf804452ca503456dc1f691
efg.river <efg.river@gmail.com>
2018-01-19 20:39:58 +0900

    first serverless handler

---
+++ src/handlers/index.js
@@ -0,0 +1,22 @@
+import version from '../chrome/version';
+
+export default async function handler (event, context, callback) {
+    let responseBody;
+
+    console.log('Getting version info.');
+
+    try {
+        responseBody = await version();
+    } catch (error) {
+        console.error('Error getting version info');
+        return callback(error);
+    }
+
+    return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(responseBody),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
